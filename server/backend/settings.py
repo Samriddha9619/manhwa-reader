e33c8ea56d1551f6a21c9f131e5d895b6715e62f
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-11-02 15:48:44 +0530

    a

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -126,4 +126,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
