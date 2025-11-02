2833a55e9e7d8dd29f568a6be314279946cf7f6b
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-11-02 16:01:13 +0530

    a

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -12,6 +12,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://manhwa-reader-production.up.railway.app',
+    'https://*.railway.app',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
